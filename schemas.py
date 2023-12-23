5c56283defe0cc70a1aa29c300ee743132468a21
Alexander Morosow <alexander.morosow@gmail.com>
2023-12-23 21:45:53 +0100

    remove email validation

--- schemas.py
+++ schemas.py
@@ -1,14 +1,45 @@
+from typing import Generic, Optional
 from beanie import PydanticObjectId
-from fastapi_users import schemas
+from fastapi_users import models
+from fastapi_users.schemas import CreateUpdateDictModel
 
 
-class UserRead(schemas.BaseUser[PydanticObjectId]):
+class BaseUser(CreateUpdateDictModel, Generic[models.ID]):
+    """Base User model."""
+
+    id: models.ID
+    email: str
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    class Config:
+        orm_mode = True
+
+
+class BaseUserCreate(CreateUpdateDictModel):
+    email: str
+    password: str
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class BaseUserUpdate(CreateUpdateDictModel):
+    password: Optional[str] = None
+    email: Optional[str] = None
+    is_active: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+    is_verified: Optional[bool] = None
+
+
+class UserRead(BaseUser[PydanticObjectId]):
     pass
 
 
-class UserCreate(schemas.BaseUserCreate):
+class UserCreate(BaseUserCreate):
     pass
 
 
-class UserUpdate(schemas.BaseUserUpdate):
+class UserUpdate(BaseUserUpdate):
     pass
